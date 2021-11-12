2a1901ba56a5ae430f914e2c236b13449d9b215b
Katie Pundt <kzpundt@gmail.com>
2021-11-11 23:39:32 -0800

    update file path to import AgeCalculator

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-import { AgeCalculator } from "./ageCalculator";
+import { AgeCalculator } from "./js/ageCalculator";
 
 $("form#ageInput").submit(function(event){
   event.preventDefault();
