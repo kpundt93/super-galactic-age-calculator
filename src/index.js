1e1c60ac74e841b41e6772163c14e3a72d333717
Katie Pundt <kzpundt@gmail.com>
2021-11-11 22:08:52 -0800

    add UI logic to display planet age

--- src/index.js
+++ src/index.js
@@ -4,3 +4,13 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import { AgeCalculator } from "./ageCalculator";
 
+$("form#ageInput").submit(function(event){
+  event.preventDefault();
+  const age = $("#age").val();
+  let calc = new AgeCalculator(age);
+  $("p#mercury").text("You are " + calc.calculator("Mercury") + " years old on Mercury!");
+  $("p#venus").text("You are " + calc.calculator("Venus") + " years old on Venus!");
+  $("p#mars").text("You are " + calc.calculator("Mars") + " years old on Mars!");
+  $("p#jupiter").text("You are " + calc.calculator("Jupiter") + " years old on Jupiter!");
+  $("div#planets").show();
+});
